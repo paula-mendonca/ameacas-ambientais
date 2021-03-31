b1e55337ee9b0d6deacb99e21c6bb484961b34b5
ppoliveire <oliveira.ppaula@outlook.com>
2021-03-30 22:32:33 -0300

    Uploading my files.

---
+++ app/src/main/java/com/paulamendonca/ameaasambientaissqliteparte1/AddAmeacaAmbiental.java
@@ -0,0 +1,14 @@
+package com.paulamendonca.ameaasambientaissqliteparte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAmeacaAmbiental extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ameaca_ambiental);
+    }
+}
